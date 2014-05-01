3597d890853640e5a5c0aafc8997cb8de348c553
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2014-05-01 07:49:32 +0530

    code added

---
+++ Backup-RavenDB.ps1
@@ -0,0 +1,21 @@
+﻿<#
+ # This function will back up my local instance of a database called Employees
+#>
+
+function Backup-RavenDB() {
+ 
+ # Location of the Raven.Smuggler.exe on my pc
+ $ravenSmugglerEXE = "C:\RavenDB-Build-2375\Smuggler\Raven.Smuggler.exe"
+
+ # The Url where my local instance of RavenDB is currently running
+ $ravenURL = "http://gsuttiepc:8080/databases/employees"
+
+ # Folder where I want my backup to end up
+ $BackupFilename = "c:\RavenDBBackups\employees.dump.raven" 
+    	       
+ # Run Smuggler.exe
+ & $ravenSmugglerEXE out $ravenUrl $BackupFilename
+}
+
+# Now call the simple function
+Backup-RavenDB
